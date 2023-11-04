603f87417f75f2cc49cb14c7301597c93a8a965f
e52348ds <daniel.stainer@manchester.ac.uk>
2023-11-04 21:28:44 +0000

    Window duplication complete

--- popupGame.js
+++ popupGame.js
@@ -3,19 +3,34 @@ const driver = () => {
     const button = document.getElementById("popup")
     const popup_window = () => {
         console.log("Popup Window Function Activated")
-        const width = 300;
-        const height = 200;
+        const minWidth = 100;
+        const minHeight = 100;
+        const width = Math.trunc(Math.random()*(window.innerWidth - minWidth)) + minWidth;
+        const height = Math.trunc(Math.random()*(window.innerHeight - minHeight)) + minHeight;
         const leftmost_position = Math.trunc(Math.random()*(window.innerWidth-width));
         const topmost_position = Math.trunc(Math.random()*(window.innerHeight-height));
-
+        /*
         console.log("window.width: " + window.innerWidth)
         console.log("window.height: " + window.innerHeight)
         console.log("Leftmost position: " + leftmost_position)
         console.log("Topmost position: " + topmost_position)
-        
-        const myWindow = window.open("", "", `width=${width},height=${height},left=${leftmost_position},top=${topmost_position}`);
-        console.log("String passed into Window.open: " + `width=${width},height=${height},left=${leftmost_position},top=${topmost_position}`)
+        */
+        const popupWindow = window.open("", "", `width=${width},height=${height},left=${leftmost_position},top=${topmost_position},popup=true`);
+        //console.log("String passed into Window.open: " + `width=${width},height=${height},left=${leftmost_position},top=${topmost_position}`)
+        /*
+        if window.location.href = <old url>:
+            window.location.href = "<new url name>"
+        otherwise,
+            do nothing
+        */
+        popupWindow.addEventListener("beforeunload", duplicate_window)
     };
+    
+    const duplicate_window = (event) => {
+        popup_window();
+        popup_window();
+        event.currentTarget.close();
+    }
     button.addEventListener("click", popup_window)
 }
 
